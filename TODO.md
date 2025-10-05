f09f6d77703b3d3d6aca3fd7c34da5791386b43d
Ziemwoit <Ziesmow@proton.me>
2025-10-05 13:48:55 +0200

    Added one thing to TODO.md and modified config for better  clearity

--- TODO.md
+++ TODO.md
@@ -1,2 +1,3 @@
 1. change tech to subs 
 2. get the nice description of the video  :)
+3. nierozdrabniać tak .cache tylko central folder i podfoldery
--- config.sh
+++ config.sh
@@ -1,9 +1,13 @@
 echo "config for yt" 
+
 mkdir -p ~/.cache/yt/
+
 echo "done"
 echo "config for ytlink ecosystem"
+
 mkdir -p ~/.cache/ytlinks/ 
 touch ~/.cache/ytlinks/youtube.links
 touch ~/.cache/ytlinks/youtube.description
+
 echo "done"
 echo "configuration ended, I wish good using :)"
